239a6f6eabe2addd90785ca7d33e052e188ff31b
shivam sharma <ds3822054@gmail.com>
2026-02-26 22:36:36 +0530

    add new paragraph

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # java-Developer
 and maintaining high-performance
 <br>
-<h1>hello shivam walcome to my java</h1>
+<h1>hello walcome to my java</h1>
---
+++ first.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class first {
+   public first() {
+   }
+
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      int var2 = var1.nextInt();
+      if (var2 == 1) {
+         System.out.println("namaste");
+      } else if (var2 == 2) {
+         System.out.println("ram ram ji2");
+      } else if (var2 == 3) {
+         System.out.println("radhe radhe");
+      } else if (var2 == 4) {
+         System.out.println("jai shree krishna ");
+      } else {
+         System.out.println("invelid number");
+      }
+
+   }
+}
